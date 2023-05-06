0828a93c5e0bb453cb224493845bb436b33c3e67
Viccky20 <102639526+Viccky20@users.noreply.github.com>
2023-05-06 23:50:40 +0530

    Database Updated And Movie Updated

--- moviebook.sql
+++ moviebook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2023 at 02:56 PM
+-- Generation Time: May 06, 2023 at 08:18 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,42 @@ SET time_zone = "+00:00";
 --
 -- Database: `moviebook`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_movie`
+--
+
+CREATE TABLE `add_movie` (
+  `id` int(25) NOT NULL,
+  `movie_name` varchar(100) NOT NULL,
+  `directer` varchar(100) NOT NULL,
+  `release_date` varchar(100) NOT NULL,
+  `categroy` varchar(100) NOT NULL,
+  `language` varchar(100) NOT NULL,
+  `you_tube_link` varchar(250) NOT NULL,
+  `show` varchar(100) NOT NULL,
+  `action` varchar(100) NOT NULL,
+  `decription` varchar(300) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `add_movie`
+--
+
+INSERT INTO `add_movie` (`id`, `movie_name`, `directer`, `release_date`, `categroy`, `language`, `you_tube_link`, `show`, `action`, `decription`, `image`, `status`) VALUES
+(9, 'Ant-Man and the Wasp: Quantumania', 'Peyton Reed\r\n\r\n', '17 February 2023', 'Action', 'English', 'https://www.youtube.com/watch?v=ZlNFpri-Y40', '18:00, 21:00', 'upcoming', 'Scott Lang and Hope Van Dyne are dragged into the Quantum Realm, along with Hope\'s parents and Scott\'s daughter Cassie.', 'Ant-man and the wasp quantumania.jpg', 0),
+(10, 'Guardians-of-galaxy3', 'James Gunn\r\n\r\n', '5 May 2023', 'Comedy', 'English', 'https://www.youtube.com/watch?v=JqcncLPi9zw', '18:00 , 21:00', 'running', 'Still reeling from the loss of Gamora, Peter Quill rallies his team to defend the universe and one of their own - a mission that could mean the end of the Guardians if not successful.', 'Guardians-of-galaxy3.jpg', 1),
+(11, 'Avatar-2', 'James Cameron', '16 December 2022', 'Science Fiction', 'English', 'https://www.youtube.com/watch?v=d9MyW72ELq0', '21:00 , 24:00', 'running', 'Avatar: The Way of Water reaches new heights and explores undiscovered depths as James Cameron returns to the world of Pandora in this emotionally packed action adventure. \r\n', 'Avatar-2.jpg', 1),
+(12, 'Baloch', 'Prakash Janardan Pawar', '5 October 2022', 'Historical', 'Marathi', 'https://www.youtube.com/watch?v=Ri7NZb3qPfk', '12:00 , 3:00', 'running', 'Loosely based on historic events post the Battle of Panipat, \'Baloch\' is about the struggles of the Marathas being transported to Afghanistan as bonded labourers.', 'Baloch.jpg', 1),
+(13, 'Shreshaah', 'Vishnuvardhan', '12 August 2021', 'Biographical War', 'Hindi', 'https://www.youtube.com/embed/Q0FTXnefVBA', '', 'upcoming', 'The life of Indian army captain Vikram Batra, awarded with the Param Vir Chakra, India\'s highest award for valour for his actions during the 1999 Kargil War.', 'shershah.jpg', 1),
+(14, 'Tanaji', 'Om Raut', '10th January 2020.', ' Historical', 'Hindi', 'https://www.youtube.com/embed/cffAGIYTEHU', '18:00,15:15', 'running', '                Gulshan Kumar, T-Series & Ajay Devgn ffilms Presents official trailer of the most awaited bollywood movie TANHAJI -THE UNSUNG WARRIOR in 3D, Directed by Om Raut, will release on 10th January 2020.\r\nTanhaji- The Unsung Warrior is an Indian biographical period drama film based on the l', 'tanaji.jpeg', 1),
+(15, 'Playing With Fire ', 'Andrea Sedlackova', '6 November 2019', 'Comedy', 'English', 'https://www.youtube.com/embed/fd5GlZUpfaM', '21:15', 'running', '                Playing with Fire is a 2019 American family comedy film directed by Andy Fickman from a screenplay by Dan Ewen and Matt Lieberman based on a story by Ewen. The film stars John Cena, Keegan-Michael Key, John Leguizamo, Dennis Haysbert, Brianna Hildebrand and Judy Greer, and follows a ', 'movieposter_en.jpg', 1),
+(16, 'Golmaal Again', 'Rohit Shetty', '20 October 2017', 'Action', 'Hindi', 'https://www.youtube.com/embed/VgQUwsUHdqc', '', 'upcoming', 'Five orphan men return to the orphanage they grew up in to attend their mentor\'s funeral. However, they encounter the ghost of their childhood friend, Khushi, and help her attain salvation.', 'golmaal_again.jpg', 1),
+(17, 'Ved', 'Riteish Deshmukh', '14 January 2023', 'Romantic', 'Marathi', 'https://www.youtube.com/watch?v=OM9Mm-V-feM', '19:00', 'Running', 'Ved ( transl. Madness) is a 2022 Indian Marathi-language romantic drama film directed by Riteish Deshmukh, in his directorial debut, and produced by Genelia D\'Souza, both of whom also star. Ashok Saraf and Jiya Shankar play supporting roles. It is a remake of the 2019 Telugu language romantic drama,', 'Ved.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -42,9 +78,123 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`id`, `name`, `email`, `password`, `is_active`) VALUES
 (1, 'Sakshi', 'Sakshidivate@gmail.com', 'Sakshi', '1');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(10) NOT NULL,
+  `uid` int(10) NOT NULL,
+  `movie` varchar(100) NOT NULL,
+  `show_time` varchar(100) NOT NULL,
+  `seat` varchar(100) NOT NULL,
+  `totalseat` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `payment_date` varchar(100) NOT NULL,
+  `booking_date` varchar(100) NOT NULL,
+  `card_name` varchar(100) NOT NULL,
+  `card_number` varchar(25) NOT NULL,
+  `ex_date` varchar(100) NOT NULL,
+  `cvv` int(5) NOT NULL,
+  `custemer_id` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `uid`, `movie`, `show_time`, `seat`, `totalseat`, `price`, `payment_date`, `booking_date`, `card_name`, `card_number`, `ex_date`, `cvv`, `custemer_id`) VALUES
+(1, 1, 'Chaal Jeevi Laiye', '15:00', 'G1,G2,D1,D2', '4', '500', 'Wed-09-21 ', 'Thu-09-21 ', 'pratik d', '7896', '2021-09-30', 789, 1869901767),
+(2, 1, 'Tanaji', '15:15', 'F7,F8,E7,E8,A7,A8', '6', '1200', 'Thu-09-21 ', 'Fri-09-21 ', 'pratik d', '145260', '2021-09-30', 349, 1890244096),
+(3, 2, 'Chaal Jeevi Laiye', '15:00', 'I5,I6,H5,H6,G5,G6', '6', '600', 'Thu-09-21 ', 'Fri-09-21 ', 'parthiv', '45456845565', '2021-10-23', 455, 560041981),
+(4, 3, 'Chaal Jeevi Laiye', '15:00', 'I7,I8', '2', '200', 'Thu-09-21 ', 'Fri-10-21 ', 'rushabh', '545656', '2021-09-29', 545, 447698228),
+(5, 3, 'Chaal Jeevi Laiye', '18:15', 'G9', '1', '100', 'Thu-09-21 ', 'Fri-10-21 ', 'rushabh', '565464', '2021-09-10', 655, 2080652377);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `massage` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `massage`) VALUES
+(1, 'pratik', 'prati@gmail.com', 'Inox Theatre is widely use now days.'),
+(2, 'parth', 'part@gmail.com', 'You can easily book your Tickets anywhere in city .'),
+(3, 'vrushti', 'vrusht@gmail.com', 'you easily choose your sheets.'),
+(4, 'yash', 'yas@gmail.com', 'Also customer service is vary good.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `theater_show`
+--
+
+CREATE TABLE `theater_show` (
+  `id` int(25) NOT NULL,
+  `show` varchar(100) NOT NULL,
+  `theater` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `theater_show`
+--
+
+INSERT INTO `theater_show` (`id`, `show`, `theater`) VALUES
+(1, '21:00', '1'),
+(2, '15:00', '1'),
+(3, '18:00', '1'),
+(4, '18:15', '2'),
+(5, '15:15', '2'),
+(6, '21:15', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(25) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` int(10) NOT NULL,
+  `city` varchar(25) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `mobile`, `city`, `password`, `image`) VALUES
+(1, 'pratik', 'prati@gmail.com', 2147483647, 'Surendranagar', '4550', ''),
+(2, 'parthiv', 'parthi@gmail.com', 2147483647, 'WADHWAN', '78963', ''),
+(3, 'rushabh', 'rushab@gmail.com', 1198875215, 'joravarnagar', '147852', ''),
+(4, 'hetanshi', 'hetansh@gmail.com', 1234567890, 'fggfrg', '0', ''),
+(5, 'dharmin', 'dharm@gmail.com', 2147483647, 'surat', 'hitesh', 'eye candy wallpapers 6 by deadpxl.jpg');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `add_movie`
+--
+ALTER TABLE `add_movie`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `admin`
@@ -54,14 +204,68 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `theater_show`
+--
+ALTER TABLE `theater_show`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `add_movie`
+--
+ALTER TABLE `add_movie`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `theater_show`
+--
+ALTER TABLE `theater_show`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
